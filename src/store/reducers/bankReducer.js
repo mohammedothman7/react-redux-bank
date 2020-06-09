5f39528ce1eb857fdd75ce83dd00a732f589758f
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-09 18:52:23 -0400

    Finished session one of review

--- src/store/reducers/bankReducer.js
+++ src/store/reducers/bankReducer.js
@@ -1,14 +1,58 @@
 // Initial State
-const initialState = {};
+const initialState = {
+  balance: 0,
+};
 
 // Actions Types
+const DEPOSIT_FIFTY = 'DEPOSIT_FIFTY';
+const DEPOSIT_ONE_HUNDRED = 'DEPOSIT_ONE_HUNDRED';
+const WITHDRAW_FIFTY = 'WITHDRAW_FIFTY';
+const WITHDRAW_ONE_HUNDRED = 'WITHDRAW_ONE_HUNDRED';
 
 // Action Creators
+export const depositFiftyActionCreator = () => ({
+  type: DEPOSIT_FIFTY,
+});
+
+export const depositOneHundredActionCreator = () => ({
+  type: DEPOSIT_ONE_HUNDRED,
+});
+
+export const withdrawFiftyActionCreator = () => ({
+  type: WITHDRAW_FIFTY,
+});
+
+export const withdrawOneHundredActionCreator = () => ({
+  type: WITHDRAW_ONE_HUNDRED,
+});
 
 // Reducer
 const bankReducer = (state = initialState, action) => {
   switch (action.type) {
     // Your cases here
+    case DEPOSIT_FIFTY:
+      return {
+        ...state,
+        balance: state.balance + 50,
+      };
+
+    case DEPOSIT_ONE_HUNDRED:
+      return {
+        ...state,
+        balance: state.balance + 100,
+      };
+
+    case WITHDRAW_FIFTY:
+      return {
+        ...state,
+        balance: state.balance - 50,
+      };
+
+    case WITHDRAW_ONE_HUNDRED:
+      return {
+        ...state,
+        balance: state.balance - 100,
+      };
 
     default:
       return state;
